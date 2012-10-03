d78a3d069ed52451af0741ef35bebc51434ec8d1
Gerhard Lazu <gerhard@lazu.co.uk>
2012-10-03 19:37:57 +0100

    No longer requiring chef-extensions

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,12 +1,5 @@
 install = (node[:phantomjs][:action] == "install")
-
-if defined?(Chef::Extensions)
-  wan_up = Chef::Extensions.wan_up?
-else
-  # Should be part of Chef really...
-  # grab it here https://github.com/gchef/chef-extensions
-  wan_up = "unknown"
-end
+wan_up = `ping -c 1 -W 1 google.com`.index(/1 (?:packets )?received/)
 
 if install
   remote_file "#{node[:phantomjs][:srcdir]}/#{node[:phantomjs][:source_file]}" do
