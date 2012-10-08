7d4d4c91354c8b6f4829b19d4317788fcf02f4d2
Gerhard Lazu <gerhard@lazu.co.uk>
2012-10-08 16:08:25 +0100

    Upgraded phantomjs to latest stable

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 maintainer        "Gerhard Lazu"
 maintainer_email  "gerhard@lazu.co.uk"
 license           "Apache 2.0"
-description       "Installs phantomjs 1.6.1 from binary build"
-version           "0.1.0"
+description       "Installs latest stable phantomjs from binary build"
+version           "0.1.1"
 
 recipe "phantomjs", "Installs phantomjs"
 
