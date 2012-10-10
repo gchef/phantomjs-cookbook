53f71126eab3f95b776e69936a04290bc5180178
Gerhard Lazu <gerhard@lazu.co.uk>
2012-10-10 08:56:48 +0100

    If WAN is down, DNS resolution won't work

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,5 @@
 install = (node[:phantomjs][:action] == "install")
-wan_up = `ping -c 1 -W 1 google.com`.index(/1 (?:packets )?received/)
+wan_up = `ping -c 1 -W 1 8.8.8.8`.index(/1 (?:packets )?received/)
 
 if install
   remote_file "#{node[:phantomjs][:srcdir]}/#{node[:phantomjs][:source_file]}" do
